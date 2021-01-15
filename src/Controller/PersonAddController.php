3a9bc68b2173bbf41ea0ee97821c87548912b7d0
elena100880 <100880kr@gmail.com>
2021-01-15 11:41:10 +0100

    Migrating Person table to DB

--- src/Controller/PersonAddController.php
+++ src/Controller/PersonAddController.php
@@ -6,44 +6,34 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-use App\Entity\Product;
-use App\Form\Type\ProductType;
+use App\Entity\Person;
+use App\Form\Type\PersonType;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\ORM\EntityManagerInterface;
 
 
 class PersonAddController extends AbstractController
-{
-    
+{    
     public function person_add (Request $request)
     {
         $person = new Person();
-                
-        $form = $this->createForm (PersonType::class, $product)
-                ->add('public_date', DateType::class, [
-                                'label'=>'Date of publication',
-                                'widget' => 'single_text',
-                                                 
-                                
-                                ] )
-            ->add('save', SubmitType::class, ['label'=>'Add the product']);
+        $form = $this->createForm (PersonType::class, $person)
+                            ->add('save', SubmitType::class, ['label'=>'Add the Person']);
          
         $form->handleRequest($request);
        
         if ($form->isSubmitted()) {
-
-            
-                               
+                          
             $productManager = $this->getDoctrine()->getManager();
-            $productManager->persist($product);
+            $productManager->persist($person);
             $productManager->flush();
             
             return $this->redirectToRoute('product');
         }
         else 
         {
-            $contents = $this->renderView('product_add/product_add.html.twig',
+            $contents = $this->renderView('person_add/person_add.html.twig',
                     [
                         'form' => $form->createView(),
                     ],
